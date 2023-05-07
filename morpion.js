660e2ed94bdace4f08e59754299908624b592425
Axiaa <91245292+Axiaaa@users.noreply.github.com>
2023-05-07 10:21:27 +0200

    v0.2.2
    - Ajout de la possibilité de rentrer un nom pour les 2 joueurs. 
    
    - Fix de l'erreur où on pouvait changer le signe déjà en place dans une case.
    
    - Fix du bouton joueur qui passait un tour. 
    
    - Le bouton jouer réinitialise maintenant le jeu.

--- morpion.js
+++ morpion.js
@@ -1,32 +1,73 @@
 let joueur = 0
+let joueur1_nom = "Joueur 1"
+let joueur2_nom = "Joueur 2"
+let statut_jeu = 0
 
 function init() {
     if (joueur == 0 ) {
-        document.getElementById("txt_joueur").innerHTML = "A joueur 1 de jouer !";
+        document.getElementById("txt_joueur").innerHTML = "A " + joueur1_nom + " de jouer !";
         joueur = 1
     } else if (joueur == 1) {
-        document.getElementById("txt_joueur").innerHTML = "A joueur 2 de jouer !";
+        document.getElementById("txt_joueur").innerHTML = "A " + joueur2_nom + " de jouer !";
         joueur = 0
     }
 
     for (let i = 1; i < 10; i++) {
         document.getElementById("case" + i).innerHTML = "";
-        document.onclick = "";
+
     }
 
+    statut_jeu = 1
 } 
 
 
 
 function Case(number) {
     let boutton = document.getElementById("case" + number);
-  
-    if (joueur === 0) {
-      boutton.innerHTML = "X";
-      joueur = 1;
-    } else if (joueur === 1) {
-      boutton.innerHTML = "O";
-      joueur = 0;
+    if (statut_jeu === 0) {
+        null
+    } else if (statut_jeu === 1) {
+            
+    if (boutton.innerText !== "X") {
+        if (boutton.innerText !== "O") {
+            if (joueur === 0) {
+            boutton.innerHTML = "X";
+            document.getElementById("txt_joueur").innerHTML = "A " + joueur1_nom + " de jouer !";
+            joueur = 1;
+                } 
+            else if (joueur === 1) {
+            boutton.innerHTML = "O";
+            document.getElementById("txt_joueur").innerHTML = "A " + joueur2_nom + " de jouer !";
+            joueur = 0;
+                }
+        }
     }
-    boutton.onclick = null;
+    
+    // for (let i = 1; i < 9; i++) {
+    //     while (document.getElementById("case" + i).innerText !== "") {}
+    //      document.getElementById("bouton_jouer").innerText = "Recommencer"
+    // }
+
+    }
 }
+
+
+function fin_jeu() {
+
+
+    
+}
+
+function player_name() {
+    joueur1_nom = document.getElementById("j1").value;
+    if (document.getElementById("j1").value === "") { 
+        joueur1_nom = "Joueur 1"
+    }
+    console.log(document.getElementById("j1").value)
+
+    joueur2_nom = document.getElementById("j2").value;
+    if (document.getElementById("j2").value === "") { 
+        joueur2_nom = "Joueur 2"
+    }
+    console.log(document.getElementById("j2").value)
+  }
